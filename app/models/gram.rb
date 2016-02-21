57c4a8826e1d130a17a83c8f5f02f89709917bb4
Winston <winston.yongwei@gmail.com>
2016-02-21 15:41:32 +0800

    Add basic image upload using cloudinary and carrier wave.

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,3 +1,5 @@
 class Gram < ActiveRecord::Base
   validates_presence_of :title, :content
+
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220131304) do
+ActiveRecord::Schema.define(version: 20160221071827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160220131304) do
     t.string   "country_code"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "image"
   end
 
 end
