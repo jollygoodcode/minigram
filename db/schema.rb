ae0ee2dd39ce8d3300ad83346c090bebcbab3ec8
Winston <winston.yongwei@gmail.com>
2016-02-26 12:33:23 +0800

    Simple user.

--- db/schema.rb
+++ db/schema.rb
@@ -25,4 +25,11 @@ ActiveRecord::Schema.define(version: 20160418150045) do
     t.string   "image_url"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
