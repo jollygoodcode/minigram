4a4723f0472eeb893d1bd849d524a6fb9da6e60e
Winston <winston.yongwei@gmail.com>
2016-03-01 23:06:55 +0800

    Simple CRD for users.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email))
+
+    if @user.save
+      redirect_to @user, notice: 'You have successfully created a user!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+end
